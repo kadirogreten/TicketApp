d6c870b17a160b0cf37e7f31855517e9c5661dad
RasulSonmez <58372444+RasulSonmez@users.noreply.github.com>
2019-12-02 23:21:39 +0300

    firmrepository de firmaya select sorgusu eklendi

--- ServiceLayer/Firm/FirmRepository.cs
+++ ServiceLayer/Firm/FirmRepository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,32 @@ namespace ServiceLayer
 {
     public class FirmRepository
     {
+        public static Firm GetFirmById(int id)
+        {
+            string commandText = $"SELECT [Id],[FirmName],[Address],FROM [dbo].[Firm] where [Id]={id}";
+
+            var cmd = SqlConnectionExtension.ConnectToDb(commandText);
+
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Firm firm = new Firm();
+
+            while (reader.Read())
+            {
+                firm = new Firm
+                {
+                    Id = reader.GetInt32(0),
+                    FirmName = reader.GetString(1),
+                    Address = reader.GetString(2),
+                    
+                };
+
+            }
+            cmd.Connection.Close();
+            cmd.Connection.Dispose();
+
+            return firm;
+        }
     }
 }
