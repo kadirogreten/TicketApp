ef5f2afdd97980cbddcf17e72088549b507ea443
RasulSonmez <58372444+RasulSonmez@users.noreply.github.com>
2019-12-03 23:01:05 +0300

    firm ya firma yı getirme ve firmaya insert sorgu methotları eklendi

--- ServiceLayer/Firm/FirmRepository.cs
+++ ServiceLayer/Firm/FirmRepository.cs
@@ -10,13 +10,14 @@ namespace ServiceLayer
 {
     public class FirmRepository
     {
+        //Firm ID sini getiren sorgu methodu
         public static Firm GetFirmById(int id)
         {
+            //Firmanın id si ile firmanın tüm bilgileri sorgulandı.
             string commandText = $"SELECT [Id],[FirmName],[Address],FROM [dbo].[Firm] where [Id]={id}";
-
+            //sqlconnectionextension helper ile db bağlantısı sağlandı.
             var cmd = SqlConnectionExtension.ConnectToDb(commandText);
-
-
+            //reader ile ile sorgu okuması yapıldı.
             SqlDataReader reader = cmd.ExecuteReader();
 
             Firm firm = new Firm();
@@ -28,14 +29,73 @@ namespace ServiceLayer
                     Id = reader.GetInt32(0),
                     FirmName = reader.GetString(1),
                     Address = reader.GetString(2),
-                    
                 };
 
             }
             cmd.Connection.Close();
             cmd.Connection.Dispose();
-
             return firm;
         }
+
+
+        //Firm yı getiren sorgu methodu
+        public static List<Firm> GetFirms()
+        {
+            string commandText = $"SELECT [Id],[FirmName],[Address], FROM [dbo].[Firm]";
+
+            var cmd = SqlConnectionExtension.ConnectToDb(commandText);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Firm firm = new Firm();
+            List<Firm> firms = new List<Firm>();
+
+            while (reader.Read())
+            {
+                firm = new Firm
+                {
+                    Id = reader.GetInt32(0),
+                    FirmName = reader.GetString(1),
+                    Address = reader.GetString(2),
+                };
+
+                firms.Add(firm);
+            }
+            cmd.Connection.Close();
+            cmd.Connection.Dispose();
+            return firms;
+        }
+
+        //firm ya intert sorgusu eklendi.
+        public static int InsertFirm(Firm firm)
+        {
+            string commandText = $"INSERT INTO [dbo].[Firm]([FirmName],[Address])VALUES('{firm.FirmName}','{firm.Address}')";
+
+            int data = 0;
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                cmd = SqlConnectionExtension.ConnectToDb(commandText);
+
+                data = cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cmd.Connection.Close();
+                cmd.Connection.Dispose();
+            }
+
+            return data;
+        }
+
+
+
     }
+
 }
